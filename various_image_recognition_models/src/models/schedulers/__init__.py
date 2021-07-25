b032536263850b68e9c848a80790c7f280f66483
中井優 <nakaiyuu@nakaiyuunoMacBook-Pro.local>
2021-07-25 11:08:32 +0900

    edit scheduler name

--- various_image_recognition_models/src/models/schedulers/__init__.py
+++ various_image_recognition_models/src/models/schedulers/__init__.py
@@ -4,13 +4,13 @@ from typing import Any, Callable, Dict
 from . import (cosine_scheduler, linear_scheduler, step_scheduler)
 
 schedulers: Dict[str, Callable[[Any, argparse.Namespace], Any]] = {
-    'cosine_scheduler': cosine_scheduler.create_scheduler,
-    'linear_scheduler': linear_scheduler.create_scheduler,
-    'step_scheduler': step_scheduler.create_optimizer,
+    'cosine': cosine_scheduler.create_scheduler,
+    'linear': linear_scheduler.create_scheduler,
+    'step': step_scheduler.create_optimizer,
 }
 
 scheduler_options: Dict[str, Callable[[argparse.ArgumentParser], argparse.ArgumentParser]] = {
-    'cosine_scheduler': lambda x: x,
-    'linear_scheduler': lambda x: x,
-    'step_scheduler': step_scheduler.scheduler_modify_commandline_options,
+    'cosine': lambda x: x,
+    'linear': lambda x: x,
+    'step': step_scheduler.scheduler_modify_commandline_options,
 }
