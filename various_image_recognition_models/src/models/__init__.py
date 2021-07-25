e508600e40f7c3d1910ad4cbee664c3d806461ce
中井優 <nakaiyuu@nakaiyuunoMacBook-Pro.local>
2021-07-25 10:10:31 +0900

    add references & fix options

--- various_image_recognition_models/src/models/__init__.py
+++ various_image_recognition_models/src/models/__init__.py
@@ -3,8 +3,12 @@ from typing import Callable, Dict
 
 from .abstract_model import AbstractModel
 
+from . import simple_model
+
 models: Dict[str, Callable[[argparse.Namespace], AbstractModel]] = {
+    'simple': simple_model.create_model,
 }
 
 model_options: Dict[str, Callable[[argparse.ArgumentParser], argparse.ArgumentParser]] = {
+    'simple': simple_model.model_modify_commandline_options,
 }
